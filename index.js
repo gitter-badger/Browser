8249f44993fe860da20d0678a4e824ccb4f279a3
Dominic Barnes <dominic@dbarnes.info>
2016-06-19 13:47:29 -0700

    formatting existing code with standard

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
+'use strict'
 
-'use strict';
-
-let buffer = require('mako-buffer');
-let copy = require('mako-copy');
-let css = require('mako-css');
-let debug = require('debug')('mako-browser');
-let defaults = require('defaults');
-let js = require('mako-js');
-let output = require('mako-output');
-let sourcemaps = require('mako-sourcemaps');
-let stat = require('mako-stat');
-let symlink = require('mako-symlink');
-let write = require('mako-write');
-
+let buffer = require('mako-buffer')
+let copy = require('mako-copy')
+let css = require('mako-css')
+let debug = require('debug')('mako-browser')
+let defaults = require('defaults')
+let js = require('mako-js')
+let output = require('mako-output')
+let sourcemaps = require('mako-sourcemaps')
+let stat = require('mako-stat')
+let symlink = require('mako-symlink')
+let write = require('mako-write')
 
 module.exports = function (options) {
-  debug('initialize %j', options);
+  debug('initialize %j', options)
   let config = defaults(options, {
     cssExtensions: null,
     forceCopy: false,
@@ -27,15 +25,15 @@ module.exports = function (options) {
     sourceMaps: false,
     symlink: false,
     write: true
-  });
+  })
 
-  return function browser(mako) {
-    let assets = [ css.images, css.fonts ];
+  return function browser (mako) {
+    let assets = [ css.images, css.fonts ]
 
     if (config.read) {
-      debug('adding read plugins');
-      mako.use(stat([ 'js', 'json', 'css', assets ]));
-      mako.use(buffer([ 'js', 'json', 'css' ]));
+      debug('adding read plugins')
+      mako.use(stat([ 'js', 'json', 'css', assets ]))
+      mako.use(buffer([ 'js', 'json', 'css' ]))
     }
 
     mako.use(js({
@@ -43,35 +41,35 @@ module.exports = function (options) {
       extensions: config.jsExtensions,
       resolveOptions: config.resolveOptions,
       sourceMaps: !!config.sourceMaps
-    }));
+    }))
 
     mako.use(css({
       extensions: config.cssExtensions,
       resolveOptions: config.resolveOptions,
       sourceMaps: !!config.sourceMaps
-    }));
+    }))
 
     if (config.sourceMaps) {
       mako.use(sourcemaps([ 'js', 'css' ], {
         inline: config.sourceMaps === 'inline'
-      }));
+      }))
     }
 
     if (config.write) {
-      debug('adding write plugins');
-      mako.use(output([ 'js', 'css', assets ], { dir: config.output }));
-      mako.use(write([ 'js', 'css' ]));
+      debug('adding write plugins')
+      mako.use(output([ 'js', 'css', assets ], { dir: config.output }))
+      mako.use(write([ 'js', 'css' ]))
 
       if (config.sourceMaps) {
-        mako.use(output('map', { dir: config.output }));
-        mako.use(write('map'));
+        mako.use(output('map', { dir: config.output }))
+        mako.use(write('map'))
       }
 
       if (config.symlink) {
-        mako.use(symlink(assets));
+        mako.use(symlink(assets))
       } else {
-        mako.use(copy(assets, { force: config.forceCopy }));
+        mako.use(copy(assets, { force: config.forceCopy }))
       }
     }
-  };
-};
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,94 +1,95 @@
+/* eslint-env mocha */
 
-'use strict';
+'use strict'
 
-let browser = require('..');
-let chai = require('chai');
-let fs = require('fs');
-let mako = require('mako');
-let path = require('path');
-let rm = require('rimraf').sync;
-let vm = require('vm');
+let browser = require('..')
+let chai = require('chai')
+let fs = require('fs')
+let mako = require('mako')
+let path = require('path')
+let rm = require('rimraf').sync
+let vm = require('vm')
 
-chai.use(require('chai-as-promised'));
-let assert = chai.assert;
-let fixture = path.resolve.bind(path, __dirname, 'fixtures');
+chai.use(require('chai-as-promised'))
+let assert = chai.assert
+let fixture = path.resolve.bind(path, __dirname, 'fixtures')
 
 afterEach(function () {
-  rm(fixture('**/build'));
-});
+  rm(fixture('**/build'))
+})
 
 describe('browser plugin', function () {
   it('should build a simple project', function () {
-    let root = fixture('simple');
-    let js = fixture('simple/index.js');
-    let css = fixture('simple/index.css');
-    let builder = mako({ root }).use(browser());
+    let root = fixture('simple')
+    let js = fixture('simple/index.js')
+    let css = fixture('simple/index.css')
+    let builder = mako({ root }).use(browser())
 
     return builder.build(js, css).then(function (build) {
-      let jsFile = build.tree.findFile(js);
+      let jsFile = build.tree.findFile(js)
       // js file execs and returns the right value
-      assert.strictEqual(exec(jsFile), true);
+      assert.strictEqual(exec(jsFile), true)
       // output location is what we expected
-      assert.strictEqual(jsFile.path, fixture('simple/build/index.js'));
+      assert.strictEqual(jsFile.path, fixture('simple/build/index.js'))
 
-      let cssFile = build.tree.findFile(css);
+      let cssFile = build.tree.findFile(css)
       // css file at least contains a substring we expect (not super-reliable)
-      assert.include(cssFile.contents.toString(), 'background-color: green;');
+      assert.include(cssFile.contents.toString(), 'background-color: green;')
       // output location is what we expected
-      assert.strictEqual(cssFile.path, fixture('simple/build/index.css'));
-    });
-  });
+      assert.strictEqual(cssFile.path, fixture('simple/build/index.css'))
+    })
+  })
 
   it('should build a project with nested imports', function () {
-    let root = fixture('nested');
-    let js = fixture('nested/index.js');
-    let css = fixture('nested/index.css');
-    let builder = mako({ root }).use(browser());
+    let root = fixture('nested')
+    let js = fixture('nested/index.js')
+    let css = fixture('nested/index.css')
+    let builder = mako({ root }).use(browser())
 
     return builder.build(js, css).then(function (build) {
-      let jsFile = build.tree.findFile(js);
+      let jsFile = build.tree.findFile(js)
       // js file execs and returns the right value
-      assert.strictEqual(exec(jsFile), 4);
+      assert.strictEqual(exec(jsFile), 4)
 
-      let cssFile = build.tree.findFile(css);
+      let cssFile = build.tree.findFile(css)
       // css file at least contains a substring we expect (not super-reliable)
-      assert.include(cssFile.contents.toString(), 'text-decoration: underline;'); // from index.css
-      assert.include(cssFile.contents.toString(), 'box-sizing: border-box;'); // from base.css
-    });
-  });
+      assert.include(cssFile.contents.toString(), 'text-decoration: underline;') // from index.css
+      assert.include(cssFile.contents.toString(), 'box-sizing: border-box;') // from base.css
+    })
+  })
 
   it('should build a project with 3rd-party imports', function () {
-    let root = fixture('vendor');
-    let js = fixture('vendor/index.js');
-    let css = fixture('vendor/index.css');
-    let builder = mako({ root }).use(browser());
+    let root = fixture('vendor')
+    let js = fixture('vendor/index.js')
+    let css = fixture('vendor/index.css')
+    let builder = mako({ root }).use(browser())
 
     return builder.build(js, css).then(function (build) {
-      let jsFile = build.tree.findFile(js);
+      let jsFile = build.tree.findFile(js)
       // js file execs and returns the right value
-      assert.deepEqual(exec(jsFile), [ 'a', 'a', 'a' ]);
+      assert.deepEqual(exec(jsFile), [ 'a', 'a', 'a' ])
 
-      let cssFile = build.tree.findFile(css);
+      let cssFile = build.tree.findFile(css)
       // css file at least contains a substring we expect (not super-reliable)
-      assert.include(cssFile.contents.toString(), 'normalize.css v3.0.3');
-    });
-  });
+      assert.include(cssFile.contents.toString(), 'normalize.css v3.0.3')
+    })
+  })
 
   it('should copy linked assets to the output directory', function () {
-    let root = fixture('assets');
-    let css = fixture('assets/index.css');
-    let image = fixture('assets/logo.png');
-    let builder = mako({ root }).use(browser());
+    let root = fixture('assets')
+    let css = fixture('assets/index.css')
+    let image = fixture('assets/logo.png')
+    let builder = mako({ root }).use(browser())
 
     return builder.build(css).then(function (build) {
-      let file = build.tree.findFile(image);
-      assert.strictEqual(file.path, fixture('assets/build/logo.png'));
-      assert.isTrue(fs.existsSync(file.path));
-    });
-  });
+      let file = build.tree.findFile(image)
+      assert.strictEqual(file.path, fixture('assets/build/logo.png'))
+      assert.isTrue(fs.existsSync(file.path))
+    })
+  })
 
-  // TODO: test the options
-});
+// TODO: test the options
+})
 
 /**
  * Executes the given code, returning it's return value.
@@ -96,6 +97,6 @@ describe('browser plugin', function () {
  * @param {String} code  The source code to run. (ie: the result of a build)
  * @return {*}
  */
-function exec(file) {
-  return vm.runInNewContext(file.contents)(file.id);
+function exec (file) {
+  return vm.runInNewContext(file.contents)(file.id)
 }
